d894c1c68e7e624c8973b4b91eae7a3870518f7d
Adrian Sanchez del C <qbanguy7@gmail.com>
2012-09-30 23:06:40 -0700

    Fixed save() method from original poster

--- classes/upload.php
+++ classes/upload.php
@@ -22,24 +22,77 @@ class Upload extends \Fuel\Core\Upload {
 	 */
 	public static function save()
 	{
-		$args = func_get_args();
-		$file = $args[0];
-		$save_as = $args[1];
+		// files to save
+		$files = array();
+		
+		// Straight from Upload.php code
+		// check for parameters
+		if (func_num_args())
+		{
+			foreach(func_get_args() as $param)
+			{
+				// string => new path to save to
+				if (is_string($param))
+				{
+					$path = $param;
+				}
+				// array => list of $files indexes to save
+				elseif(is_array($param))
+				{
+					$files = array();
+					foreach($param as $key)
+					{
+						if (isset(static::$files[(int) $key]))
+						{
+							$files[(int) $key] = static::$files[(int) $key];
+						}
+					}
+				}
+				// integer => files index to save
+				elseif(is_numeric($param))
+				{
+					if (isset(static::$files[$param]))
+					{
+						$files[$param] = static::$files[$param];
+					}
+				}
+			}
+		}
+		else
+		{
+			// save all files
+			$files = static::$files;
+		}
 
 		// get access keys
 		$access_key_id = static::$config['access_key_id'];
 		$secret_access_key = static::$config['secret_access_key'];
 		$bucket_name = static::$config['bucket_name'];
 		$enable_ssl = static::$config['enable_ssl'];
-
+		
 		// create s3 object
-		$s3 = new \S3($access_key_id, $secret_access_key, $enable_ssl);
-
-		// path to save the files to
-		$path = static::$config['path'];
-
-		// files to save
-		$files = array();
-		$s3->putObject(file_get_contents($file), $bucket_name, $save_as, \S3::ACL_PUBLIC_READ);
+		$s3 = new \S3($access_key_id, $secret_access_key);
+		
+		foreach($files as $file)
+		{
+			//always randomize file
+			$filename = md5(serialize($file).time());
+			
+			$save_as = array(
+				static::$config['prefix'],
+				$filename,
+				static::$config['suffix'],
+				'',
+				'.',
+				empty(static::$config['extension']) ? $file['extension'] : static::$config['extension']
+			);
+			
+			$save_as = implode('', $save_as);
+			
+			//lets put the file!
+			$s3->putObject(file_get_contents($file['file']), $bucket_name, $save_as, \S3::ACL_PUBLIC_READ);
+		}
+		
+		
 	}
 }
